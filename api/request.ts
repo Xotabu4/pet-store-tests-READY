efa5148a6a212b1e4f072c0591e720e237e7fe50
Oleksandr Khotemskyi <Xotabu4@gmail.com>
2021-01-31 16:49:33 +0200

    draft

---
+++ api/request.ts
@@ -0,0 +1,27 @@
+import type { Options, Method, Response } from 'got';
+import got from 'got';
+
+export class JsonRequest {
+    protected options: any = {
+        responseType: "json"
+    };
+    public url(url: string | URL): this {
+        this.options.url = url
+        return this
+    }
+    public method(method: Method): this {
+        this.options.method = method
+        return this
+    }
+    public searchParams(searchParams: Options["searchParams"]): this {
+        this.options.searchParams = searchParams
+        return this
+    }
+    public body(body: any): this {
+        this.options.json = body;
+        return this;
+    };
+    public async send() {
+        return got<any>(this.options)
+    }
+}
--- test/pets.test.ts
+++ test/pets.test.ts
@@ -1,5 +1,5 @@
 import { strict as assert } from 'assert'
-import { PetController } from '../api/controller/pet.controller';
+import { PetController } from '../api/controller/pet-no-builder.controller';
 
 const pet = new PetController()
 
@@ -13,14 +13,16 @@ describe('Pet', () => {
     it('can be received by status', async function () {
         let petResp = await pet.findByStatus('available')
         assert(petResp.length > 0)
+        assert(petResp.every((pet: any) => pet.status == 'available'))
 
         petResp = await pet.findByStatus('pending')
         assert(petResp.length > 0)
+        assert(petResp.every((pet: any) => pet.status == 'pending'))
 
         petResp = await pet.findByStatus('sold')
         assert(petResp.length > 0)
+        assert(petResp.every((pet: any) => pet.status == 'sold'))
 
-        // Multiple statuses are applicable as well
         petResp = await pet.findByStatus(['pending', 'available'])
         assert(petResp.length > 0)
         assert(petResp.some((pet: any) => pet.status == 'available'))
@@ -32,6 +34,27 @@ describe('Pet', () => {
         const petResp = await pet.findByTags('tag1')
         assert(petResp.length > 0)
         assert(petResp.some((pet: any) => pet.tags.some((tag: any) => tag.name == 'tag1')))
+    })
+
+    it('can be added, updated, and deleted', async function () {
+        // {
+        //     "id": 0,
+        //     "category": {
+        //       "id": 0,
+        //       "name": "string"
+        //     },
+        //     "name": "doggie",
+        //     "photoUrls": [
+        //       "string"
+        //     ],
+        //     "tags": [
+        //       {
+        //         "id": 0,
+        //         "name": "string"
+        //       }
+        //     ],
+        //     "status": "available"
+        //   }
     })
     
 }) 

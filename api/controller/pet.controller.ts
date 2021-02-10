99fe8e7e235ef2bc5363c016faf434bf7bd2d4fc
Oleksandr Khotemskyi <xotabu4@gmail.com>
2021-02-10 09:08:17 +0200

    removed auto validation

--- api/controller/pet.controller.ts
+++ api/controller/pet.controller.ts
@@ -1,11 +1,11 @@
 import { URLSearchParams } from 'url';
 import { definitions, operations } from '../../.temp/types'
-import { JsonRequestWithValidation } from '../request';
+import { JsonRequest } from 'http-req-builder';
 
 export class PetController {
     async addNew(pet: Omit<definitions['Pet'], "id">) {
         return (
-            await new JsonRequestWithValidation()
+            await new JsonRequest()
                 .url(`http://93.126.97.71:10080/api/pet`)
                 .method('POST')
                 .body(pet)
@@ -15,7 +15,7 @@ export class PetController {
 
     async update(pet: definitions['Pet']) {
         return (
-            await new JsonRequestWithValidation()
+            await new JsonRequest()
                 .url(`http://93.126.97.71:10080/api/pet`)
                 .method('PUT')
                 .body(pet)
@@ -25,7 +25,7 @@ export class PetController {
 
     async delete(id: number | string) {
         return (
-            await new JsonRequestWithValidation()
+            await new JsonRequest()
                 .url(`http://93.126.97.71:10080/api/pet/${id}`)
                 .method('DELETE')
                 .send<{ message: string }>()
@@ -34,7 +34,7 @@ export class PetController {
 
     async findByTags(tags: string | string[]) {
         return (
-            await new JsonRequestWithValidation()
+            await new JsonRequest()
                 .url(`http://93.126.97.71:10080/api/pet/findByTags`)
                 .searchParams(new URLSearchParams({ tags }))
                 .send<operations['findPetsByTags']['responses']['200']['schema']>()
@@ -43,7 +43,7 @@ export class PetController {
 
     async findByStatus(status: string | string[]) {
         return (
-            await new JsonRequestWithValidation()
+            await new JsonRequest()
                 .url(`http://93.126.97.71:10080/api/pet/findByStatus`)
                 .searchParams(new URLSearchParams({ status }))
                 .send<operations['findPetsByStatus']['responses']['200']['schema']>()
@@ -52,9 +52,20 @@ export class PetController {
 
     async getById(id: number | string) {
         return (
-            await new JsonRequestWithValidation()
+            await new JsonRequest()
                 .url(`http://93.126.97.71:10080/api/pet/${id}`)
                 .send<operations['getPetById']['responses']['200']['schema']>()
         ).body;
+
+        // {
+        //     allErrors: true,
+        //     verbose: true,
+        //     jsonPointers: true,
+        //     formats: {
+        //         double: "[+-]?\\d*\\.?\\d+",
+        //         int32: /^(-?\d{1,9}|-?1\d{9}|-?20\d{8}|-?21[0-3]\d{7}|-?214[0-6]\d{6}|-?2147[0-3]\d{5}|-?21474[0-7]\d{4}|-?214748[012]\d{4}|-?2147483[0-5]\d{3}|-?21474836[0-3]\d{2}|214748364[0-7]|-214748364[0-8])$/,
+        //         int64: /^\d+$/,
+        //     },
+        // },
     }
 }
--- api/controller/store.controller.ts
+++ api/controller/store.controller.ts
@@ -1,9 +1,9 @@
 import { operations } from "../../.temp/types";
-import { JsonRequestWithValidation } from "../request";
+import { JsonRequest } from 'http-req-builder';
 
 export class StoreController {
     async getInventory() {
-        return (await new JsonRequestWithValidation()
+        return (await new JsonRequest()
             .url(`http://93.126.97.71:10080/api/store/inventory`)
             .send<operations['getInventory']['responses']['200']['schema']>()
         ).body

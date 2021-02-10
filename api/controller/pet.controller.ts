dfbf8c44b47637d7088873e7b911455bee68f496
Oleksandr Khotemskyi <xotabu4@gmail.com>
2021-02-10 17:29:38 +0200

    fix

--- api/controller/pet.controller.ts
+++ api/controller/pet.controller.ts
@@ -1,6 +1,33 @@
 import { URLSearchParams } from 'url';
 import { definitions, operations } from '../../.temp/types'
 import { JsonRequest } from 'http-req-builder';
+import Ajv from 'ajv';
+import SwaggerParser from "@apidevtools/swagger-parser";
+
+async function loadAPISpec() {
+    return SwaggerParser.dereference('http://93.126.97.71:10080/api/swagger.json');
+}
+
+function validate(schema: any, body: any) {
+    const ajv = new Ajv({
+        strict: false,
+        allErrors: true,
+        verbose: true,
+        formats: {
+            double: "[+-]?\\d*\\.?\\d+",
+            int32: /^(-?\d{1,9}|-?1\d{9}|-?20\d{8}|-?21[0-3]\d{7}|-?214[0-6]\d{6}|-?2147[0-3]\d{5}|-?21474[0-7]\d{4}|-?214748[012]\d{4}|-?2147483[0-5]\d{3}|-?21474836[0-3]\d{2}|214748364[0-7]|-214748364[0-8])$/,
+            int64: /^\d+$/,
+        },
+    });
+    const validate = ajv.compile(schema);
+    const valid = validate(body);
+    if (!valid) {
+        throw new Error(`Swagger validation errors: ${JSON.stringify({
+            // body: body,
+            validationErrors: validate.errors
+        }, null, 2)}`)
+    }
+}
 
 export class PetController {
     async addNew(pet: Omit<definitions['Pet'], "id">) {
@@ -51,21 +78,18 @@ export class PetController {
     }
 
     async getById(id: number | string) {
-        return (
+        let body = (
             await new JsonRequest()
                 .url(`http://93.126.97.71:10080/api/pet/${id}`)
                 .send<operations['getPetById']['responses']['200']['schema']>()
         ).body;
 
-        // {
-        //     allErrors: true,
-        //     verbose: true,
-        //     jsonPointers: true,
-        //     formats: {
-        //         double: "[+-]?\\d*\\.?\\d+",
-        //         int32: /^(-?\d{1,9}|-?1\d{9}|-?20\d{8}|-?21[0-3]\d{7}|-?214[0-6]\d{6}|-?2147[0-3]\d{5}|-?21474[0-7]\d{4}|-?214748[012]\d{4}|-?2147483[0-5]\d{3}|-?21474836[0-3]\d{2}|214748364[0-7]|-214748364[0-8])$/,
-        //         int64: /^\d+$/,
-        //     },
-        // },
+        const apiSpec = await loadAPISpec()
+        const schema = apiSpec.paths['/pet/{petId}']['get']['responses']['200']['schema']
+
+        body = { test: 'hello' } as any
+        validate(schema, body)
+
+        return body
     }
 }

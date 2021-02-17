81752f7d613bbdd1cca3e0026c364c7f6c769b05
Oleksandr Khotemskyi <Xotabu4@gmail.com>
2021-02-17 18:46:43 +0200

    Update pet.controller.ts

--- api/controller/pet.controller.ts
+++ api/controller/pet.controller.ts
@@ -3,6 +3,33 @@ import { definitions, operations } from '../../.temp/types'
 import { JsonRequestWithValidation } from '../request';
 
 export class PetController {
+    async getById(id: number | string) {
+        return (
+            await new JsonRequestWithValidation()
+                .url(`http://93.126.97.71:10080/api/pet/${id}`)
+                .send<operations['getPetById']['responses']['200']['schema']>()
+        ).body;
+    }
+
+    async findByTags(tags: string | string[]) {
+        return (
+            await new JsonRequestWithValidation()
+                .url(`http://93.126.97.71:10080/api/pet/findByTags`)
+                .searchParams(new URLSearchParams({ tags }))
+                .send<operations['findPetsByTags']['responses']['200']['schema']>()
+        ).body;
+    }
+
+    async findByStatus(status: string | string[]) {
+        return (
+            await new JsonRequestWithValidation()
+                .url(`http://93.126.97.71:10080/api/pet/findByStatus`)
+                .searchParams(new URLSearchParams({ status }))
+                .send<operations['findPetsByStatus']['responses']['200']['schema']>()
+        ).body;
+    }
+
+
     async addNew(pet: Omit<definitions['Pet'], "id">) {
         return (
             await new JsonRequestWithValidation()
@@ -29,32 +56,6 @@ export class PetController {
                 .url(`http://93.126.97.71:10080/api/pet/${id}`)
                 .method('DELETE')
                 .send<{ message: string }>()
-        ).body;
-    }
-
-    async findByTags(tags: string | string[]) {
-        return (
-            await new JsonRequestWithValidation()
-                .url(`http://93.126.97.71:10080/api/pet/findByTags`)
-                .searchParams(new URLSearchParams({ tags }))
-                .send<operations['findPetsByTags']['responses']['200']['schema']>()
-        ).body;
-    }
-
-    async findByStatus(status: string | string[]) {
-        return (
-            await new JsonRequestWithValidation()
-                .url(`http://93.126.97.71:10080/api/pet/findByStatus`)
-                .searchParams(new URLSearchParams({ status }))
-                .send<operations['findPetsByStatus']['responses']['200']['schema']>()
-        ).body;
-    }
-
-    async getById(id: number | string) {
-        return (
-            await new JsonRequestWithValidation()
-                .url(`http://93.126.97.71:10080/api/pet/${id}`)
-                .send<operations['getPetById']['responses']['200']['schema']>()
         ).body;
     }
 }

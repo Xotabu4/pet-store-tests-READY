bd24a1a267b827f6ef21f763272ea7b4b56a7ad7
Oleksandr Khotemskyi <Xotabu4@gmail.com>
2021-05-23 21:07:13 +0300

    Added env file to repo

--- api/controller/store.controller.ts
+++ api/controller/store.controller.ts
@@ -3,13 +3,13 @@ import { BaseController } from "./base.controller";
 
 export class StoreController extends BaseController {
     async getOrderById(orderId: number | string) {
-        return (await this.Request()
+        return (await this.request()
             .url(`store/order/${orderId}`)
             .send<operations['getOrderById']['responses']['200']['schema']>()
         ).body
     }
     async placeOrder(order: Omit<definitions["Order"], "id" | "status">) {
-        return (await this.Request()
+        return (await this.request()
             .url(`store/order`)
             .method('POST')
             .body(order)
@@ -17,7 +17,7 @@ export class StoreController extends BaseController {
         ).body
     }
     async getInventory() {
-        return (await this.Request()
+        return (await this.request()
             .url(`store/inventory`)
             .send<operations['getInventory']['responses']['200']['schema']>()
         ).body

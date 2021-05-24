6de15200fbb37ea9f57e47ab507ea2f13c42ac06
Oleksandr Khotemskyi <Xotabu4@gmail.com>
2021-05-24 20:54:56 +0300

    Added allure reporter and request tracking

--- .mocharc.js
+++ .mocharc.js
@@ -4,6 +4,8 @@ module.exports = {
         'dotenv/config'
     ],
     spec: './test/**/*.test.ts',
+    reporter: 'mocha-multi-reporters',
+    reporterOptions: ['configFile=reporterConfig.json'],
     slow: 50000,
     timeout: 60000,
 }
